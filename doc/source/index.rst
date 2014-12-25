c8f0160d62de3aef83c18d57e4247da3126b04d6
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2014-12-25 23:36:14 +0100

    Initial project structure

---
+++ doc/source/index.rst
@@ -0,0 +1,22 @@
+.. pywrap documentation master file, created by
+   sphinx-quickstart on Thu Dec 25 23:33:41 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to pywrap's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+import pywrap
+
+
+if __name__ == "__main__":
+    setup(name='pywrap',
+          version=pywrap.__version__,
+          author='Alexander Fabisch',
+          author_email='afabisch@googlemail.com',
+          url='TODO',
+          description='Generator for Python and C++ wrappers',
+          long_description=open('README.rst').read(),
+          license='New BSD',
+          packages=['pywrap'],)
