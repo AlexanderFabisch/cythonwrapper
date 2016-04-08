02f5ae7cb0ba90c19216ca669f8fd3979e5e1c48
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-04-08 14:59:00 +0200

    Extract function export

--- pywrap/cython.py
+++ pywrap/cython.py
@@ -251,89 +251,15 @@ class FunctionBase(object):
         self.name = name
         self.arguments = []
 
-    def function_def(self, includes, initial_args=[], result_type=None):
-        body, args, call_args = self._input_type_conversions(
-            includes, initial_args)
-        body += self._call_cpp_function(call_args, result_type)
-        body += self._output_type_conversion(result_type)
-        return self._signature(args) + os.linesep + indent_block(body, 1)
-
     def accept(self, exporter):
         for arg in self.arguments:
             arg.accept(exporter)
-
-    def _call_cpp_function(self, call_args, result_type=None):
-        call = "self.thisptr.{fname}({args})".format(
-            fname=self.name, args=", ".join(call_args))
-        if result_type != "void":
-            call = "cdef {result_type} result = {call}".format(
-                result_type=result_type, call=call)
-        return call + os.linesep
-
-    def _signature(self, args):
-        return "def %s(%s):" % (from_camel_case(self.name), ", ".join(args))
-
-    def _input_type_conversions(self, includes, initial_args):
-        body = ""
-        call_args = []
-        args = []
-        args.extend(initial_args)
-        skip = False
-
-        for i in range(len(self.arguments)):
-            if skip:
-                skip = False
-                continue
-
-            argument = self.arguments[i]
-            cppname = "cpp_" + argument.name
-
-            args.append(argument.name)
-            call_args.append(cppname)
-
-            if is_type_with_automatic_conversion(argument.tipe):
-                body += cython_define_basic_inputarg(
-                    argument.tipe, cppname, argument.name) + os.linesep
-            elif argument.tipe == "double *":
-                includes.numpy = True
-                body += cython_define_nparray1d_inputarg(
-                    argument.tipe, cppname, argument.name)
-                call_args.append(argument.name + "_array.shape[0]")
-                skip = True
-            elif argument.tipe.startswith("vector"):
-                body += cython_define_basic_inputarg(
-                    argument.tipe, cppname, argument.name) + os.linesep
-            else:
-                raise NotImplementedError("No known conversion for type %r"
-                                          % argument.tipe)
-
-        return body, args, call_args
-
-    def _output_type_conversion(self, result_type):
-        if result_type is None or result_type == "void":
-            return ""
-        elif is_type_with_automatic_conversion(result_type):
-            return "return result" + os.linesep
-        else:
-            # TODO only works with default constructor
-            cython_classname = "Cpp%s" % result_type.split()[0]
-            return """ret = %s()
-ret.thisptr = result
-return ret
-""" % cython_classname
 
 
 class Constructor(FunctionBase):
     def __init__(self, name, class_name):
         super(self.__class__, self).__init__(name)
         self.class_name = class_name
-
-    def _call_cpp_function(self, call_args, result_type):
-        return "self.thisptr = new %s(%s)%s" % (
-            self.class_name, ", ".join(call_args), os.linesep)
-
-    def _signature(self, args):
-        return "def __init__(%s):" % ", ".join(args)
 
     def accept(self, exporter):
         super(Constructor, self).accept(exporter)
@@ -441,17 +367,18 @@ class CythonImplementationExporter:
         self.methods = []
 
     def visit_constructor(self, ctor):
-        const_str = indent_block(
-            ctor.function_def(self.includes, initial_args=["self"]), 1)
-        self.ctors.append(const_str)
+        function_def = ConstructorDefinition(
+            ctor.class_name, ctor.name, ctor.arguments, self.includes,
+            initial_args=["self"]).make()
+        self.ctors.append(indent_block(function_def, 1))
 
         self.arguments = []
 
     def visit_method(self, method):
-        method_str = indent_block(method.function_def(
-            self.includes, initial_args=["self"],
-            result_type=method.result_type), 1)
-        self.methods.append(method_str)
+        function_def = FunctionDefinition(
+            method.name, method.arguments, self.includes, ["self"],
+            method.result_type).make()
+        self.methods.append(indent_block(function_def, 1))
 
         self.arguments = []
 
@@ -460,3 +387,93 @@ class CythonImplementationExporter:
 
     def export(self):
         return self.includes.header() + os.linesep + os.linesep + self.output
+
+
+class FunctionDefinition(object):
+    def __init__(self, name, arguments, includes, initial_args, result_type):
+        self.name = name
+        self.arguments = arguments
+        self.includes = includes
+        self.initial_args = initial_args
+        self.result_type = result_type
+
+    def make(self):
+        body, args, call_args = self._input_type_conversions(
+            self.includes, self.initial_args)
+        body += self._call_cpp_function(call_args, self.result_type)
+        body += self._output_type_conversion(self.result_type)
+        return self._signature(args) + os.linesep + indent_block(body, 1)
+
+    def _call_cpp_function(self, call_args, result_type=None):
+        call = "self.thisptr.{fname}({args})".format(
+            fname=self.name, args=", ".join(call_args))
+        if result_type != "void":
+            call = "cdef {result_type} result = {call}".format(
+                result_type=result_type, call=call)
+        return call + os.linesep
+
+    def _signature(self, args):
+        return "def %s(%s):" % (from_camel_case(self.name), ", ".join(args))
+
+    def _input_type_conversions(self, includes, initial_args):
+        body = ""
+        call_args = []
+        args = []
+        args.extend(initial_args)
+        skip = False
+
+        for i in range(len(self.arguments)):
+            if skip:
+                skip = False
+                continue
+
+            argument = self.arguments[i]
+            cppname = "cpp_" + argument.name
+
+            args.append(argument.name)
+            call_args.append(cppname)
+
+            if is_type_with_automatic_conversion(argument.tipe):
+                body += cython_define_basic_inputarg(
+                    argument.tipe, cppname, argument.name) + os.linesep
+            elif argument.tipe == "double *":
+                includes.numpy = True
+                body += cython_define_nparray1d_inputarg(
+                    argument.tipe, cppname, argument.name)
+                call_args.append(argument.name + "_array.shape[0]")
+                skip = True
+            elif argument.tipe.startswith("vector"):
+                body += cython_define_basic_inputarg(
+                    argument.tipe, cppname, argument.name) + os.linesep
+            else:
+                raise NotImplementedError("No known conversion for type %r"
+                                          % argument.tipe)
+
+        return body, args, call_args
+
+    def _output_type_conversion(self, result_type):
+        if result_type is None or result_type == "void":
+            return ""
+        elif is_type_with_automatic_conversion(result_type):
+            return "return result" + os.linesep
+        else:
+            # TODO only works with default constructor
+            cython_classname = "Cpp%s" % result_type.split()[0]
+            return """ret = %s()
+ret.thisptr = result
+return ret
+""" % cython_classname
+
+
+class ConstructorDefinition(FunctionDefinition):
+    def __init__(self, class_name, name, arguments, includes, initial_args):
+        super(ConstructorDefinition, self).__init__(
+            name, arguments, includes, initial_args, result_type=None)
+        self.class_name = class_name
+
+    def _call_cpp_function(self, call_args, result_type):
+        return "self.thisptr = new %s(%s)%s" % (
+            self.class_name, ", ".join(call_args), os.linesep)
+
+    def _signature(self, args):
+        return "def __init__(%s):" % ", ".join(args)
--- pywrap/test/test_cython.py
+++ pywrap/test/test_cython.py
@@ -1,22 +1,22 @@
-from pywrap.cython import Includes, Param, FunctionBase
+from pywrap.cython import Includes, Param, FunctionDefinition
 from pywrap.utils import assert_equal_linewise
 
 
 def test_simple_function_def():
-    fun = FunctionBase("testfun")
     assert_equal_linewise(
-        fun.function_def(Includes("test_module"), initial_args=["self"],
-                         result_type="void"),
+        FunctionDefinition(
+            "testfun", [], Includes("test_module"),
+            initial_args=["self"], result_type="void").make(),
         """def testfun(self):
     self.thisptr.testfun()
 """)
 
 
 def test_array_arg_function_def():
-    fun = FunctionBase("testfun")
-    fun.arguments = [Param("a", "double *"), Param("aSize", "unsigned int")]
-    testfun = fun.function_def(Includes("test_module"), initial_args=["self"],
-                               result_type="void")
+    testfun = FunctionDefinition(
+        "testfun", [Param("a", "double *"), Param("aSize", "unsigned int")],
+        Includes("test_module"), initial_args=["self"], result_type="void"
+        ).make()
     assert_equal_linewise(testfun,
         """def testfun(self, a):
     cdef np.ndarray[double, ndim=1] a_array = np.asarray(a)
