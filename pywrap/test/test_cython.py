ddd968c3aa8da2528da208af76f4ab2611049521
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-04-27 23:00:20 +0200

    Test missing module name

--- pywrap/test/test_cython.py
+++ pywrap/test/test_cython.py
@@ -1,25 +1,7 @@
-from pywrap.exporter import MethodDefinition
-from pywrap.cython import TypeInfo
-from pywrap.parser import Param
-from pywrap.ast import Includes, Param
-from pywrap.utils import assert_equal_linewise, lines
-from pywrap.defaultconfig import Config
+from pywrap.cython import make_cython_wrapper
+from nose.tools import assert_raises
 
 
-def test_simple_function_def():
-    assert_equal_linewise(
-        MethodDefinition(
-            "Testclass", "testfun", [], Includes(),
-            "void", TypeInfo({}), Config()).make(),
-            lines("cpdef testfun(Testclass self):",
-                  "    self.thisptr.testfun()"))
-
-
-def test_array_arg_function_def():
-    testfun = MethodDefinition(
-        "Testclass", "testfun", [Param("a", "double *"),
-                                 Param("aSize", "unsigned int")],
-        Includes(), "void", TypeInfo({}), Config()).make()
-    assert_equal_linewise(testfun,
-        lines("cpdef testfun(Testclass self, np.ndarray[double, ndim=1] a):",
-              "    self.thisptr.testfun(&a[0], a.shape[0])"))
+def test_missing_modulename():
+    assert_raises(ValueError, make_cython_wrapper,
+                  ["test1.hpp", "test2.hpp"], [], None)
---
+++ pywrap/test/test_exporter.py
@@ -0,0 +1,24 @@
+from pywrap.exporter import MethodDefinition
+from pywrap.cython import TypeInfo
+from pywrap.ast import Includes, Param
+from pywrap.utils import assert_equal_linewise, lines
+from pywrap.defaultconfig import Config
+
+
+def test_simple_function_def():
+    assert_equal_linewise(
+        MethodDefinition(
+            "Testclass", "testfun", [], Includes(),
+            "void", TypeInfo({}), Config()).make(),
+            lines("cpdef testfun(Testclass self):",
+                  "    self.thisptr.testfun()"))
+
+
+def test_array_arg_function_def():
+    testfun = MethodDefinition(
+        "Testclass", "testfun", [Param("a", "double *"),
+                                 Param("aSize", "unsigned int")],
+        Includes(), "void", TypeInfo({}), Config()).make()
+    assert_equal_linewise(testfun,
+        lines("cpdef testfun(Testclass self, np.ndarray[double, ndim=1] a):",
+              "    self.thisptr.testfun(&a[0], a.shape[0])"))
