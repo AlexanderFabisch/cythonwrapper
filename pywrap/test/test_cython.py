685c3f5eadd3ae8b6e60a9d5839a4fd4518effa0
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-04-28 22:53:27 +0200

    Test missing config

--- pywrap/test/test_cython.py
+++ pywrap/test/test_cython.py
@@ -5,3 +5,8 @@ from nose.tools import assert_raises
 def test_missing_modulename():
     assert_raises(ValueError, make_cython_wrapper,
                   ["test1.hpp", "test2.hpp"], [], None)
+
+
+def test_missing_config():
+    assert_raises(ValueError, make_cython_wrapper,
+                  "test.hpp", [], custom_config="doesnotexist.py")
