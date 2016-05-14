90e2dde5df2de30368d3ce8bc601aa9d06618454
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-05-14 18:47:39 +0200

    Increase test coverage of exporters

--- pywrap/test/test_exporter.py
+++ pywrap/test/test_exporter.py
@@ -1,12 +1,13 @@
 from pywrap.exporter import (MethodDefinition, SetterDefinition,
                              GetterDefinition, ConstructorDefinition,
-                             FunctionDefinition)
+                             FunctionDefinition, CythonDeclarationExporter)
 from pywrap.cython import TypeInfo
-from pywrap.ast import Includes, Param
+from pywrap.ast import (Includes, Param, Function, Clazz, Constructor, Method,
+                        Field)
 from pywrap.utils import lines
 from pywrap.defaultconfig import Config
 from pywrap.ast import Field
-from nose.tools import assert_multi_line_equal
+from nose.tools import assert_equal, assert_multi_line_equal
 
 
 def test_simple_function_def():
@@ -79,5 +80,115 @@ def test_function_def():
         lines(
             "cpdef my_fun():",
             "    cpp.myFun()"
+        )
+    )
+
+
+def test_function_def_with_another_cppname():
+    fun = FunctionDefinition("myFunInt", [], Includes(), "void", TypeInfo(),
+                             Config(), cppname="myFun").make()
+    assert_multi_line_equal(
+        fun,
+        lines(
+            "cpdef my_fun_int():",
+            "    cpp.myFun()"
+        )
+    )
+
+
+def test_function_decl():
+    fun = Function("test.hpp", "", "myFun", "void")
+    ignored_fun = Function("test.hpp", "", "myFun", "void")
+    ignored_fun.ignored = True
+    exporter = CythonDeclarationExporter(Includes(), Config())
+    exporter.visit_function(fun)
+    exporter.visit_function(ignored_fun)
+    exporter.visit_ast(None)
+    decl = exporter.export()
+    assert_multi_line_equal(
+        decl.strip(),
+        lines(
+            "cdef extern from \"test.hpp\" namespace \"\":",
+            "    void myFun()"
+        )
+    )
+
+
+def test_class_decl():
+    clazz = Clazz("test.hpp", "", "MyClass")
+    exporter = CythonDeclarationExporter(Includes(), Config())
+    exporter.visit_class(clazz)
+    exporter.visit_ast(None)
+    decl = exporter.export()
+    assert_multi_line_equal(
+        decl.strip(),
+        lines(
+            "cdef extern from \"test.hpp\" namespace \"\":",
+            "    cdef cppclass MyClass:",
+            "        pass"
+        )
+    )
+
+
+def test_ctor_decl():
+    clazz = Clazz("test.hpp", "", "MyClass")
+    ctor = Constructor("MyClass")
+    ignored_ctor = Constructor("MyClass")
+    ignored_ctor.ignored = True
+    exporter = CythonDeclarationExporter(Includes(), Config())
+    exporter.visit_constructor(ctor)
+    exporter.visit_constructor(ignored_ctor)
+    exporter.visit_class(clazz)
+    exporter.visit_ast(None)
+    decl = exporter.export()
+    assert_multi_line_equal(
+        decl.strip(),
+        lines(
+            "cdef extern from \"test.hpp\" namespace \"\":",
+            "    cdef cppclass MyClass:",
+            "        MyClass()"
+        )
+    )
+
+
+def test_method_decl():
+    clazz = Clazz("test.hpp", "", "MyClass")
+    method = Method("myMethod", "void", "MyClass")
+    ignored_method = Method("", "", "")
+    ignored_method.ignored = True
+    exporter = CythonDeclarationExporter(Includes(), Config())
+    exporter.visit_param(Param("myParam", "double"))
+    exporter.visit_method(method)
+    exporter.visit_method(ignored_method)
+    exporter.visit_class(clazz)
+    exporter.visit_ast(None)
+    decl = exporter.export()
+    assert_multi_line_equal(
+        decl.strip(),
+        lines(
+            "cdef extern from \"test.hpp\" namespace \"\":",
+            "    cdef cppclass MyClass:",
+            "        void myMethod(double myParam)"
+        )
+    )
+
+
+def test_field_decl():
+    clazz = Clazz("test.hpp", "", "MyClass")
+    field = Field("myField", "double", "MyClass")
+    ignored_field = Field("myField", "double", "MyClass")
+    ignored_field.ignored = True
+    exporter = CythonDeclarationExporter(Includes(), Config())
+    exporter.visit_field(field)
+    exporter.visit_field(ignored_field)
+    exporter.visit_class(clazz)
+    exporter.visit_ast(None)
+    decl = exporter.export()
+    assert_multi_line_equal(
+        decl.strip(),
+        lines(
+            "cdef extern from \"test.hpp\" namespace \"\":",
+            "    cdef cppclass MyClass:",
+            "        double myField"
         )
     )
