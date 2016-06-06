cbdf79f38bdb8b7a8cfcc35cb7d28db9c719e94b
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-06-06 22:40:50 +0200

    Update version number

--- pywrap/__init__.py
+++ pywrap/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0"
+__version__ = "0.1"
 __description__ = "Generate Cython wrapper for C++ code automatically."
 __author__ = "Alexander Fabisch"
 __author_email__ = "afabisch@googlemail.com"
