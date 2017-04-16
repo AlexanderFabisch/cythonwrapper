ebbe09bb3a7c7ba49aeb73c33a466ea4afc703e6
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2017-04-16 23:23:08 +0200

    Refactor search for libclang

--- pywrap/libclang.py
+++ pywrap/libclang.py
@@ -25,45 +25,60 @@ def _find_clang(verbose=0):
             os.path.join(basepath, "llvm-%s" % clang_version, "lib")
         ]
         for lib_path in search_paths:
-            if not os.path.exists(lib_path):
-                continue
             if verbose >= 3:
-                print("Searching for libclang in '%s'..." % lib_path)
+                print("Searching for libclang %s in '%s'..."
+                      % (clang_version, lib_path))
 
-            lib_names = ["libclang-%s.so" % clang_version,
-                         "libclang.so.%s" % clang_version]
-            lib_filename = None
-            for lib_name in lib_names:
-                lib_file = os.path.join(lib_path, lib_name)
-                if os.path.exists(lib_file):
-                    lib_filename = lib_file
-                    break
+            if not os.path.exists(lib_path):
+                if verbose >= 3:
+                    print("Directory does not exist.")
+                continue
+
+            lib_filename = _find_lib(lib_path, clang_version)
             if verbose >= 3:
                 if lib_filename is None:
-                    print("No library found.")
+                    print("Library not found.")
                 else:
                     print("Found libclang at '%s'." % lib_filename)
             if lib_filename is None:
                 continue
 
-            incdir_pattern = os.path.join(
-                basepath, "clang/%s.?/include/" % clang_version)
-            clang_incdirs = list(glob.glob(incdir_pattern))
-            if len(clang_incdirs) != 1:
-                raise ImportError(
-                    "Could not find one clang include directory. "
-                    "Checked '%s'." % incdir_pattern)
-            clang_incdir = clang_incdirs[0]
+            clang_incdir = _find_include_directory(basepath, clang_version)
             if verbose >= 3:
                 print("Found clang include directory at '%s'."
-                        % clang_incdir)
+                      % clang_incdir)
 
             cindex.Config.set_library_path(lib_path)
 
             return clang_version, clang_incdir
+
     raise ImportError("Could not find a valid installation of libclang-dev. "
                       "Only versions %s are supported at the moment."
                       % SUPPORTED_VERSIONS)
+
+
+def _find_lib(lib_path, clang_version):
+    lib_names = ["libclang-%s.so" % clang_version,
+                 "libclang.so.%s" % clang_version]
+    lib_filename = None
+    for lib_name in lib_names:
+        lib_file = os.path.join(lib_path, lib_name)
+        if os.path.exists(lib_file):
+            lib_filename = lib_file
+            break
+    return lib_filename
+
+
+def _find_include_directory(basepath, clang_version):
+    incdir_pattern = os.path.join(
+        basepath, "clang/%s.?/include/" % clang_version)
+    clang_incdirs = list(glob.glob(incdir_pattern))
+    if len(clang_incdirs) != 1:
+        raise ImportError(
+            "Could not find exactly one clang include directory. "
+            "Checked pattern '%s'." % incdir_pattern)
+    clang_incdir = clang_incdirs[0]
+    return clang_incdir
 
 
 # This must be done globally and exactly once:
